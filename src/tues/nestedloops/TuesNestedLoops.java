13f9af5141d4b53c63c699bcbdf749c8771485ca
John Haley <john@hashback.io>
2018-03-07 12:00:07 -0700

    Initial commit

---
+++ src/tues/nestedloops/TuesNestedLoops.java
@@ -0,0 +1,35 @@
+package tues.nestedloops;
+
+/**
+ *
+ * @author john
+ */
+public class TuesNestedLoops {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        final int LENGTH = 4;
+        for (int i = 0; i <= LENGTH; i++) {
+            for (int j = 0; j <= LENGTH; j++) {
+                if (i % LENGTH == 0 || j % LENGTH == 0) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        /*
+        
+        *       *****   *           *****       *****
+        **      *   *   **         *****       *   *
+        ***     *   *   * *       *****       *   *
+        ****    *   *   *  *     *****       *   *
+        *****   *****   *****   *****       *****
+        
+        */
+    }
+
+}
